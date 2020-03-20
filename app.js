465b67fe0a352f19147c89949c8e62ee1f5933f8
Okka Linardi <okka.linardi@gmail.com>
2020-03-20 14:12:34 +0700

    Added mongoose connect to skeleton

--- app.js
+++ app.js
@@ -7,6 +7,21 @@ const app = express()
 const router = require('./routes')
 const cors = require('cors')
 const errorHandler = require('./middlewares/errorHandler')
+const mongoose = require('mongoose')
+const mongodb = process.env.MONGO_ATLAS || 'mongodb://localhost:27017'
+let dbName
+
+if (process.env.NODE_ENV == 'development' || process.env.NODE_ENV == 'test') {
+    dbName = 'splitter-' + process.env.NODE_ENV
+}
+
+mongoose.connect(mongodb+dbName, {useNewUrlParser:true, useUnifiedTopology: true})
+.then(success => {
+    console.log('connected to MongoDb on ' + mongodb+dbName)
+})
+.catch(err => {
+    console.log(err)
+})
 
 app
     .use(cors())
