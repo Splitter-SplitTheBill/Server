25ea2313ad03c271832a4b3c704a68cc4aa946a4
Okka Linardi <okka.linardi@gmail.com>
2020-03-20 20:21:42 +0700

    Resolved Routing Conflict

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,8 @@
 const route = require('express').Router()
-<<<<<<< Updated upstream
 const UserRouter = require('./UserRouter')
-
-route.use('/users', UserRouter)
-=======
 const transactions = require('./transactions')
 
+route.use('/users', UserRouter)
 route.use('/transactions', transactions)
->>>>>>> Stashed changes
 
 module.exports = route
