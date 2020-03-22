8fdbfe05d4f0868397eb3b62fec5e846565ed6d2
Onesinus SPT <onesinus231@gmail.com>
2020-03-22 18:52:23 +0700

    Menambahkan middleware unggah

--- routes/event.js
+++ routes/event.js
@@ -1,9 +1,10 @@
 const route             = require('express').Router()
 const EventController   = require('../controllers/EventController');
+const upload            = require('../middlewares/unggah')
 
 route.get('/', EventController.listEvents);
 route.get('/:id', EventController.findEventById);
-route.post('/', EventController.addEvent);
+route.post('/', upload.single('photo'), EventController.addEvent);
 route.put('/:id', EventController.updateEvent);
 route.delete('/:id', EventController.deleteEvent);
 
